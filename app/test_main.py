d74259bff2a3b7e3878029917b5d1b6492ce0ffe
Vlada <vlada.belayy@mail.com>
2024-08-07 20:28:23 +0300

    commit

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string_returns_true() -> None:
+    assert is_isogram("") is True
+
+
+def test_correct_word_returns_true() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_incorrect_word_returns_true() -> None:
+    assert is_isogram("look") is False
+
+
+def test_word_is_capitalized_but_it_is_repeated() -> None:
+    assert is_isogram("Adam") is False
+
